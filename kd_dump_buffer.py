87b7899959d67e1dd2d6ab9a2edfb508d4d80582
smali <test9996889@gmail.com>
2021-07-13 15:08:03 +0800

    修复 dump 插件 在 mac 11.4 系统上得问题

--- kd_dump_buffer.py
+++ kd_dump_buffer.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: By 空道
-# Created on 10:19 2015/3/6
+# Created on 10:19 2020/11/12
 
 __author__ = "TheCjw"
 
 import os
 import idc
+import platform
 from idaapi import *
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
 
 dialogKDDataDump = [
 "STARTITEM 0",         #//让第一项获得焦点
@@ -33,6 +37,61 @@ def strToHexStr(strbuf):
     for c in strbuf:
         bytestrs += ('%02x' % ord(c))
     return bytestrs
+
+def ida_dump_ask_file(file_path, ea_addr, size):
+    savepath = ask_file(1, file_path, "sava path")
+    dumpMemoryToFile(savepath, ea_addr, size)
+def dumpMemoryToFile(savepath, ea_addr, size):
+    if savepath is None:
+        print "cancel dump!"
+    else:
+        buffer = idc.GetManyBytes(ea_addr, size, False)
+        if buffer is not None:
+            with open(savepath, "wb") as f:
+                f.write(buffer)
+                f.close()
+                print("output addr:0x%08X, Size:0x%08X" % (ea_addr,size))
+                print("Saved data success", savepath)
+        else:
+            print "get memory bytes error"
+
+
+g_dumpUi = None
+
+    ## 因为 ida 的bug, 目前只找到这种变扭的实现方式,不能在ida 主线程中调用ui的api;
+def my_dump_qt5_ask_file(file_path, ea_addr, size):
+    class MyWindow(QtWidgets.QWidget):
+        def __init__(self, file_path, ea_addr, size):
+            super(MyWindow,self).__init__()
+            self.myButton = QtWidgets.QPushButton(self)
+            self.myButton.setObjectName("myButton")
+            self.myButton.setText("Test")
+            self.myButton.clicked.connect(self.msg)
+            self.file_path = file_path
+            self.ea_addr = ea_addr
+            self.size = size
+            
+
+        def msg(self):
+            file_path, filetype = QFileDialog.getSaveFileName(self,
+                                        "Dump memory",
+                                        self.file_path,
+                                        "Dump Files (*.Dump);;All Files (*)")   #设置文件扩展名过滤,注意用双分号间隔
+            
+            #print(fileName1,filetype)
+            if len(file_path) > 0:
+                dumpMemoryToFile(file_path, self.ea_addr, self.size)
+            self.close()
+    def action(add):
+        add.myButton.click()
+    global g_dumpUi
+    if(None == g_dumpUi):
+        g_dumpUi=MyWindow(file_path, ea_addr, size)
+    g_dumpUi.resize(1,1)
+    g_dumpUi.show()
+    str_inages = (g_dumpUi,)
+    t1 = threading.Thread(target=action,args=str_inages)
+    t1.start()
 
 def KDDataDumpMain():
     nStartAddres = idc.ScreenEA();
@@ -62,19 +121,12 @@ def KDDataDumpMain():
     if idc.isLoaded(StartAddresForm.value) and idc.isLoaded(StartAddresForm.value + size - 1) :
         if OutRadioForm.value == 0:
             strpath=os.path.dirname(idc.GetIdbPath()) + os.sep + "%08X-%08X.Dump"%(StartAddresForm.value, StartAddresForm.value + size)
-            savepath = idc.AskFile(1, strpath, "sava path")
-            if savepath is None:
-                print "cancel dump!"
+            #savepath = idc.AskFile(1, strpath, "sava path")
+            ##//为了兼容 mac 11.4版本
+            if(platform.platform() == 'Darwin-20.5.0-x86_64-i386-64bit'):
+                my_dump_qt5_ask_file(strpath, StartAddresForm.value, size)
             else:
-                buffer = idc.GetManyBytes(StartAddresForm.value, size, False)
-                if buffer is not None:
-                    with open(savepath, "wb") as f:
-                        f.write(buffer)
-                        f.close()
-                        print("output addr:0x%08X, Size:0x%08X" % (StartAddresForm.value,size))
-                        print("Saved data success", savepath)
-                else:
-                    print "get memory bytes error"
+                ida_dump_ask_file(strpath, StartAddresForm.value, size)
         else:
             buffer = idc.GetManyBytes(StartAddresForm.value, size, False)
             if buffer is not None:
